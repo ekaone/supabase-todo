317ececa1107544d47e4eef1722f1354531864c5
ekaone <ekaone3033@gmail.com>
2021-03-06 14:04:20 +0700

    disable providers google and github

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { supabase } from '../lib/initSupabase'
-import { Auth } from '@supabase/ui'
-import TodoList from '../components/TodoList'
+import { supabase } from "../lib/initSupabase";
+import { Auth } from "@supabase/ui";
+import TodoList from "../components/TodoList";
 
 export default function IndexPage() {
-  const { user } = Auth.useUser()
+  const { user } = Auth.useUser();
 
   return (
     <div className="w-full h-full bg-gray-300">
@@ -11,7 +11,7 @@ export default function IndexPage() {
         <div className="w-full h-full flex justify-center items-center p-4">
           <Auth
             supabaseClient={supabase}
-            providers={['google', 'github']}
+            // providers={['google', 'github']}
             socialLayout="horizontal"
             socialButtonSize="xlarge"
           />
@@ -19,14 +19,14 @@ export default function IndexPage() {
       ) : (
         <div
           className="w-full h-full flex flex-col justify-center items-center p-4"
-          style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}
+          style={{ minWidth: 250, maxWidth: 600, margin: "auto" }}
         >
           <TodoList user={supabase.auth.user()} />
           <button
             className="btn-black w-full mt-12"
             onClick={async () => {
-              const { error } = await supabase.auth.signOut()
-              if (error) console.log('Error logging out:', error.message)
+              const { error } = await supabase.auth.signOut();
+              if (error) console.log("Error logging out:", error.message);
             }}
           >
             Logout
@@ -34,5 +34,5 @@ export default function IndexPage() {
         </div>
       )}
     </div>
-  )
+  );
 }
